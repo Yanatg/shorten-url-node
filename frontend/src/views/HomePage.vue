dbeb04b94fad4b0ae716e2bd068db0f785070ffe
Yanatchara JERAJA <yanatchara.j@ku.th>
2025-04-14 17:07:49 +0700

    fix duplicate history

--- frontend/src/views/HomePage.vue
+++ frontend/src/views/HomePage.vue
@@ -3,8 +3,6 @@
   <div>
     <UrlShortener />
     <HistoryList v-if="authStore.isLoggedIn && authStore.initialCheckDone" class="mt-12"/>
-    <HistoryList v-if="authStore.isLoggedIn && authStore.initialCheckDone"/>
-
     <div v-else-if="!authStore.isLoggedIn && authStore.initialCheckDone" class="text-center mt-12 p-6 bg-blue-50 border border-blue-200 rounded-lg">
         <p class="text-gray-600">
             <router-link :to="{ name: 'Login' }" class="text-blue-600 font-semibold hover:underline">Log in</router-link> or
